59c14f0c35649124d6fe0e1a972704f62ddc506c
Jamie Goodnight <jamiegoodnight@Jamies-Air.attlocal.net>
2019-11-17 20:28:15 -0500

    :sparkles: Migrated to and working on heroku

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'adventure',
+    'adventure.apps.AdventureConfig',
     'api',
     'corsheaders',
     'rest_framework',
--- adventure/admin.py
+++ adventure/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Room, Player
+
+admin.site.site_header='Adventure Game'
+admin.site.site_title='Adventure Game Administration'
+
+admin.site.register(Room)
+admin.site.register(Player)
+
+
 # Register your models here.
