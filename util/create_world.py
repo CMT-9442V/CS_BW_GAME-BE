21dfc3ec31a5c5d8c053b92c1f1d9c46b1b9d299
Philip Johnson <phil7j@gmail.com>
2019-11-20 18:35:07 -0800

    added all channels to data

--- util/create_world.py
+++ util/create_world.py
@@ -1,47 +1,26 @@
-from django.contrib.auth.models import User
-from adventure.models import Player, Room
 
-
-Room.objects.all().delete()
-
-r_outside = Room(title="Outside Cave Entrance",
-               description="North of you, the cave mount beckons")
-
-r_foyer = Room(title="Foyer", description="""Dim light filters in from the south. Dusty
-passages run north and east.""")
-
-r_overlook = Room(title="Grand Overlook", description="""A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""")
-
-r_narrow = Room(title="Narrow Passage", description="""The narrow passage bends here from west
-to north. The smell of gold permeates the air.""")
-
-r_treasure = Room(title="Treasure Chamber", description="""You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south.""")
-
-r_outside.save()
-r_foyer.save()
-r_overlook.save()
-r_narrow.save()
-r_treasure.save()
-
-# Link rooms together
-r_outside.connectRooms(r_foyer, "n")
-r_foyer.connectRooms(r_outside, "s")
-
-r_foyer.connectRooms(r_overlook, "n")
-r_overlook.connectRooms(r_foyer, "s")
-
-r_foyer.connectRooms(r_narrow, "e")
-r_narrow.connectRooms(r_foyer, "w")
-
-r_narrow.connectRooms(r_treasure, "n")
-r_treasure.connectRooms(r_narrow, "s")
-
-players=Player.objects.all()
-for p in players:
-  p.currentRoom=r_outside.id
-  p.save()
-
+from adventure.models import Channel
+import random
+Channel.objects.all().delete()
+backgrounds=['https://i.imgur.com/WU4JRaN.jpg', 'https://i.imgur.com/fF1TtUz.jpg', 'https://i.imgur.com/u59oC5P.png']
+audio=['tv_static', 'glitched_tones', 'radio_static']
+glitchtype=['static', 'DeafultTextGlitch', 'ComicBook']
+geometry=[100,200,300]
+text=['23423', 'please', '--.-333.999----9v', '55.7558', '37.6173']
+def channel_maker(num_channels):
+    tot_channels=0
+    prev_channel=0
+    curr_channel=0
+    next_channel=1
+    while tot_channels > num_channels:
+        if next_channel <= num_channels:
+            new_channel=Channel(channel=cur_channel, background=random.choice(backgrounds), audio=random.choice(audio), glitchtype=random.choice(glitchtype), geometry=random.choice(geometry), text=random.choice(text), up_to=next_channel, down_to=prev_channel)
+            new_channel.save()
+        else:
+            next_channel=cur_channel
+            new_channel=Channel(channel=cur_channel, background=random.choice(backgrounds), audio=random.choice(audio), glitchtype=random.choice(glitchtype), geometry=random.choice(geometry), text=random.choice(text), up_to=next_channel, down_to=prev_channel)
+            new_channel.save()
+    prev_channel=cur_channel
+    cur_channel += 1
+    next_channel +=1
+channel_maker(10)
